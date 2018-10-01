cebe5fbe5543cb8f7131c135163cf03504bf0c2d
Vasiliy Saenko <saenko@mqd.me>
2018-10-01 14:46:01 +0300

    update dependencies

--- object_detection/inference.py
+++ object_detection/inference.py
@@ -1,3 +1,14 @@
+
+import sys
+sys.path.append('..')
+
+# import slim
+import os, sys, inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+sys.path.insert(0, parentdir+"/slim")
+
 import numpy as np
 import os
 import six.moves.urllib as urllib
